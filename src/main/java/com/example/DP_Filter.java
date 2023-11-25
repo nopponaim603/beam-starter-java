c91ff3880d95ddce130473b849271503eaf56ae1
noppon_aim@hotmail.com <noppon_aim@hotmail.com>
2023-11-25 13:12:40 +0700

    Datapipeline sample Filter.

---
+++ src/main/java/com/example/DP_Filter.java
@@ -0,0 +1,56 @@
+package com.example;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/// Sample from https://tour.beam.apache.org/tour/java/common-transforms/filter
+
+public class DP_Filter {
+    private static final Logger LOG = LoggerFactory.getLogger(DP_Filter.class);
+
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+        Pipeline pipeline = Pipeline.create(options);
+
+        // Create input PCollection
+        PCollection<Integer> input =
+                pipeline.apply(Create.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        // The [input] filtered with the applyTransform()
+        PCollection<Integer> output = applyTransform(input);
+
+        output.apply("Log", ParDo.of(new LogOutput<>("PCollection filtered value")));
+
+        pipeline.run();
+    }
+
+    // The method filters the collection so that the numbers are even
+    static PCollection<Integer> applyTransform(PCollection<Integer> input) {
+        return input.apply(Filter.by(number -> number % 2 == 0));
+    }
+
+    static class LogOutput<T> extends DoFn<T, T> {
+        private String prefix;
+
+        LogOutput() {
+            this.prefix = "Processing element";
+        }
+
+        LogOutput(String prefix) {
+            this.prefix = prefix;
+        }
+
+        @ProcessElement
+        public void processElement(ProcessContext c) throws Exception {
+            LOG.info(prefix + ": {}", c.element());
+        }
+    }
+}
